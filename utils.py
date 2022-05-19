8c65dd53bcff53d80202aa143108e56492f00c8d
5658www638 <2223485532@qq.com>
2022-05-19 10:48:49 +0800

    add the face detector, but not do well

--- utils.py
+++ utils.py
@@ -9,7 +9,19 @@ video_path = "/home/MyServer/data/RECOLA/recordings_video/recordings_video"
 audio_frames_path = "/home/MyServer/data/RECOLA/audio_frames"
 audio_path = "/home/MyServer/data/RECOLA/recordings_audio/recordings_audio"
 filename = "/home/MyServer/data/RECOLA/recordings_audio/recordings_audio/dev_5.wav"
-pic_path = "/home/MyServer/My_Code/MachineLearning/AutoEncoder/test.jpg"
+pic_path = "/home/MyServer/MyCode/AutoEncoder/test.jpg"
+frame_path = "/home/MyServer/data/RECOLA/video_frames/train_3/7_280.jpg"
+xml_path = "/usr/local/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_alt2.xml"
+
+def extract_face(frame, frame_buff):    # 提取单帧的人脸数据
+    face_detector = cv2.CascadeClassifier(xml_path)
+    face = face_detector.detectMultiScale(frame)
+    if len(face) != 0:
+        x, y, w, h = face[0]
+        frame = frame[x: x+w, y: y+h]
+        return frame
+    else:   # 检测不到人脸，则返回上一帧的人脸
+        return frame_buff
 
 def extract_imgs(video_path, video_frames_path):
     """
@@ -30,6 +42,7 @@ def extract_imgs(video_path, video_frames_path):
         frame_buff = 0
         while frame_exit:
             frame_exit, frame = video.read()            # 获取下一帧
+            frame = extract_face(frame, frame_buff)                 # 截取人脸
             time = video.get(cv2.CAP_PROP_POS_MSEC)     # 获取当前时间戳
 
             ex = (time // 40) -  frame_num  # 正常来说，ex应该为0
@@ -66,6 +79,9 @@ def torchaudio_test():
 
 if __name__ == "__main__":
     extract_imgs(video_path, video_frames_path)
+    # frame = cv2.imread(frame_path)
+    # frame = extract_face(frame)
+    # cv2.imwrite(pic_path, frame)
     # torchaudio_test()
     # path = "/home/MyServer/data/RECOLA/recordings_video/recordings_video/dev_5.mp4"
     # cap = cv2.VideoCapture(path)
