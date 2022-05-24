59423ddde0edfb6b7f052023ca9cf8d86e6c1d2b
5658www638 <2223485532@qq.com>
2022-05-24 19:56:29 +0800

    fix somewhere

--- functions.py
+++ functions.py
@@ -46,35 +46,34 @@ def extract_imgs(video_path, video_frames_path, model_path):
     for name in videos_names:
         full_name = os.path.join(video_path, name)
         save_path = os.path.join(video_frames_path, name[:-4])
-        
         video = cv2.VideoCapture(full_name)
         frame_exit = video.isOpened()
         frame_num = 0
         frame_name = 0
         acc = 100
+        frame_buff = 0
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
-        frame_buff = 0
         while frame_exit:
             frame_exit, frame = video.read()            # 获取下一帧
             
             if frame_exit:
                 frame, acc = me.pic_predict(device, net, frame)
                 time = video.get(cv2.CAP_PROP_POS_MSEC)     # 获取当前时间戳
-
+                
                 ex = (time // 40) -  frame_num  # 正常来说，ex应该为0
                 # 执行以下循环，说明发生了跳帧
                 for i in range(int(ex)):
                     if acc <= 70:
-                        frame_name = str(frame_num)+"_"+str(int(time-i*40)) + "_ac_" + "ex_" + ".jpg"
+                        frame_name = str(frame_num)+"_"+str(int(time-(i+1)*40)) + "_ac_" + "ex_" + ".jpg"
                     else:
-                        frame_name = str(frame_num)+"_"+str(int(time-i*40)) + "_ex_" + ".jpg"
+                        frame_name = str(frame_num)+"_"+str(int(time-(i+1)*40)) + "_ex_" + ".jpg"
                     frame_path = os.path.join(save_path, frame_name)
                     frame_num += 1
                     cv2.imwrite(frame_path, frame_buff)
-                    # print(frame_path)
+                    print(frame_name)
                 frame_buff = frame  # 保留本次的图像，供下一次如果掉帧时使用
 
                 if acc <= 70:
@@ -82,9 +81,16 @@ def extract_imgs(video_path, video_frames_path, model_path):
                 else:
                     frame_name = str(frame_num)+"_"+str(int(time)) + ".jpg"
                 frame_path = os.path.join(save_path, frame_name)
-
-                cv2.imwrite(frame_path, frame)
-                frame_num += 1
+                print(frame_name)
+                try:
+                    cv2.imwrite(frame_path, frame)
+                    frame_num += 1
+                except cv2.error:       # 会出现这个奇怪的错误
+                    send_msg("出现异常")
+                    frame_name = str(frame_num)+"_"+str(int(time)) + "_bc_" + ".jpg"
+                    cv2.imwrite(frame_path, frame_buff)
+                    print(frame_name)
+                    frame_num += 1
             
             else:
                 send_msg("完成一次")
@@ -104,20 +110,5 @@ def torchaudio_test():
 if __name__ == "__main__":
     model_path = "/home/MyServer/model_file/RetinaFace/Retinaface_model_v2/Resnet50_Final.pth"
     extract_imgs(video_path, video_frames_path, model_path)
-    # img_path = "/home/MyServer/data/RECOLA/video_frames/train_3/7074_282960.jpg"
-    # save_path = "/home/MyServer/MyCode/AutoEncoder/"
-    # device = torch.device("cuda")
-    # net = me.model_config(device, model_path)
-    # frame = cv2.imread(img_path)
-    # frame, acc = me.pic_predict(device, net, frame)
-    # print(acc)
-    # cv2.imwrite(save_path+"hh.jpg", frame)
-
-    # frame = cv2.imread(frame_path)
-    # frame = extract_face(frame)
-    # cv2.imwrite(pic_path, frame)
-    # torchaudio_test()
-    # path = "/home/MyServer/data/RECOLA/recordings_video/recordings_video/dev_5.mp4"
-    # cap = cv2.VideoCapture(path)
-    # print(cap.get(7)/cap.get(5)/60)
+    
 
